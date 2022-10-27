e34cfaa989eb68dc92483c8aa5166a376e61a106
Dat17 <tmdatsc.vl@gmail.com>
2022-10-27 23:55:21 +0700

    draw hcn va booman

--- boomGame/boomGame.pro
+++ boomGame/boomGame.pro
@@ -12,9 +12,11 @@ TARGET = boomGame
 TEMPLATE = app
 
 
-SOURCES += main.cpp\
-        mainwindow.cpp
+SOURCES += main.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  +=
 
 FORMS    += mainwindow.ui
+
+RESOURCES += \
+    res.qrc
--- boomGame/main.cpp
+++ boomGame/main.cpp
@@ -1,11 +1,38 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QGraphicsRectItem>
+#include <QGraphicsEllipseItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+#include <QGraphicsPixmapItem>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    
+
+    // create a scene
+    QGraphicsScene *scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,500,900); // set width height for scene
+
+
+    // create an item to add to the scene
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(80,500,50,90);
+
+    QGraphicsPixmapItem * pixmapItem = new QGraphicsPixmapItem();
+
+    pixmapItem->setPixmap(QPixmap(":/images/dataset/character_stand_still/character_stand_still.gif"));
+
+
+    // add the item to the scene
+    scene->addItem(rect);
+
+   // scene->addItem(pixmapItem);
+
+    // add a view
+    QGraphicsView * view = new QGraphicsView(scene);
+
+    view->show();
     return a.exec();
 }
